3ee6fa8bd93c373307e1f2786ac375758d9cccc1
Abdullah Bueno <abueno@email.sc.edu>
2022-05-17 20:32:25 -0400

    .

--- UPF_SignUp.cs
+++ UPF_SignUp.cs
@@ -7,11 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace UPF_App
 {
     public partial class Form1 : Form
     {
+        /* This part is to stop the window from flickering whenver clicking on components.
+           In order to stop the window from flickering, import the following code inside the object and
+           "using System.Runtime.InteropServices;" and "using Microsoft.Win32;"
+              {
+               SendMessage(this.Handle, WM_SETREDRAW, false, 0);
+               
+               // Whatever code you wanna run
+               SendMessage(this.Handle, WM_SETREDRAW, true, 0);
+               this.Refresh();
+              }
+           Also copy above the public Form() argument :
+            [DllImport("user32.dll")]
+            public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+            private const int WM_SETREDRAW = 11;
+         */
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+        private const int WM_SETREDRAW = 11;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,7 +48,7 @@ namespace UPF_App
 
         }
 
-  
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -46,9 +67,14 @@ namespace UPF_App
 
         private void SearchBtn_Click(object sender, EventArgs e)
         {
+            SendMessage(this.Handle, WM_SETREDRAW, false, 0);
+
             UPF_Search UPF_S = new UPF_Search();
             UPF_S.Show();
             Visible = false;
+
+            SendMessage(this.Handle, WM_SETREDRAW, true, 0);
+            this.Refresh();
         }
 
         private void button1_Click(object sender, EventArgs e)
